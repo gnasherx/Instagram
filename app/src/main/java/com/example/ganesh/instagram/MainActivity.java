9aca656a7ccdc6934539b686538f14fc7238a615
gnasherx <pawarg124@gmail.com>
2018-08-05 19:20:41 +0530

    bottom navigation with fragments done.

--- app/src/main/java/com/example/ganesh/instagram/MainActivity.java
+++ app/src/main/java/com/example/ganesh/instagram/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.ganesh.instagram;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import com.example.ganesh.instagram.viewpages.HomeFragment;
+import com.example.ganesh.instagram.viewpages.LikesFragment;
+import com.example.ganesh.instagram.viewpages.PostFragment;
+import com.example.ganesh.instagram.viewpages.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +22,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar myToolbar = findViewById(R.id.app_bar);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
+
+//        Bottom Navigation Bar
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+//        Load HomeFragment when app starts
+        loadFragment(new HomeFragment());
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = null;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.action_search:
+                        fragment = new SearchFragment();
+                        break;
+                    case R.id.action_post:
+                        fragment = new PostFragment();
+                        break;
+                    case R.id.action_like:
+                        fragment = new LikesFragment();
+                        break;
+                    case R.id.action_profile:
+                        break;
+                }
+                return loadFragment(fragment);
+            }
+        });
     }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
